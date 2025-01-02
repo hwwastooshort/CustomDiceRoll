ee46f7fc1c199334bdb552215bab2e0598f37340
hw <molle.hw@gmail.com>
2025-01-02 12:53:29 +0100

    first commit

---
+++ CustomDiceRoll/CustomDiceRollApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomDiceRollApp.swift
+//  CustomDiceRoll
+//
+//  Created by Harvey Wirth on 02.01.25.
+//
+
+import SwiftUI
+
+@main
+struct CustomDiceRollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
