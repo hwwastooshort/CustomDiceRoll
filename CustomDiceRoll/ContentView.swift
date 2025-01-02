bb23d8f3dda6f6ea461cb724c09647d0c7c7b617
hw <molle.hw@gmail.com>
2025-01-02 14:08:46 +0100

    implemented own strucst for the roll button and heading

--- CustomDiceRoll/ContentView.swift
+++ CustomDiceRoll/ContentView.swift
@@ -11,22 +11,67 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             Color.black.ignoresSafeArea()
-            
             VStack {
-                HStack {
-                    Image(systemName: "dice")
-                        .foregroundColor(.white)
-                        .font(.title)
-                    Text("CustomDiceRoller")
-                        .foregroundStyle(.white)
-                        .font(.title)
-                }
-                .padding(.top, 50) // Abstand von oben
+                Heading()
+                    .padding(.top)
                 Spacer()
+                
+                HStack {
+                    Spacer()
+                    DiceRollButton {
+                        print("Button has been pressed")
+                    }
+                    .padding()
+                }
             }
         }
     }
 }
+
+struct DiceRollButton: View {
+    var action: () -> Void
+
+    var body: some View {
+        Button(action: action) {
+            ZStack {
+                Color.white
+                    .frame(width: 100, height: 100)
+                    .cornerRadius(10)
+                VStack {
+                    Image(systemName: "dice")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 50, height: 50)
+                        .foregroundColor(.black)
+                    Text("Roll")
+                        .font(.callout)
+                        .foregroundColor(.black)
+                }
+            }
+        }
+    }
+}
+
+
+struct Heading: View {
+    var body: some View {
+        ZStack {
+            Color.white
+                .cornerRadius(15)
+                .frame(height: 100)
+                .padding(.horizontal, 20)
+            HStack {
+                Image(systemName: "dice")
+                    .foregroundColor(.black)
+                    .font(.title)
+                Text("CustomDiceRoller")
+                    .foregroundStyle(.black)
+                    .font(.title)
+            }
+        }
+    }
+}
+
 
 #Preview {
     ContentView()
