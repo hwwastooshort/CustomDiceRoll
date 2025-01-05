d570f50628c14ab80d254c518f1c5864e76e7ba3
hw <molle.hw@gmail.com>
2025-01-05 13:00:31 +0100

    Deleted mock up UI

--- CustomDiceRoll/ContentView.swift
+++ CustomDiceRoll/ContentView.swift
@@ -9,67 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Color.black.ignoresSafeArea()
-            VStack {
-                Heading()
-                    .padding(.top)
-                Spacer()
-                
-                HStack {
-                    Spacer()
-                    DiceRollButton {
-                        print("Button has been pressed")
-                    }
-                    .padding()
-                }
-            }
-        }
+        Text("Errors be gone!")
     }
 }
 
-struct DiceRollButton: View {
-    var action: () -> Void
-
-    var body: some View {
-        Button(action: action) {
-            ZStack {
-                Color.white
-                    .frame(width: 100, height: 100)
-                    .cornerRadius(10)
-                VStack {
-                    Image(systemName: "dice")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 50, height: 50)
-                        .foregroundColor(.black)
-                    Text("Roll")
-                        .font(.callout)
-                        .foregroundColor(.black)
-                }
-            }
-        }
-    }
-}
-
-struct Heading: View {
-    var body: some View {
-        ZStack {
-            Color.white
-                .cornerRadius(15)
-                .frame(height: 100)
-                .padding(.horizontal, 20)
-            HStack {
-                Image(systemName: "dice")
-                    .foregroundColor(.black)
-                    .font(.title)
-                Text("CustomDiceRoller")
-                    .foregroundStyle(.black)
-                    .font(.title)
-            }
-        }
-    }
-}
 
 
 #Preview {
